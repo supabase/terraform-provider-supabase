f0e55219ec58c7644795f5ab06b69a27df666725
Han Qiao <qiao@supabase.io>
2024-04-03 15:09:03 +0800

    feat: support configuring database settings (#56)
    * feat: support configuring database settings
    
    * chore: reuse config update methods
    
    * docs: update json schema file
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- examples/resources/supabase_settings/resource.tf
+++ examples/resources/supabase_settings/resource.tf
@@ -1,6 +1,10 @@
 resource "supabase_settings" "production" {
   project_ref = "mayuaycdtijbctgqbycg"
 
+  database = jsonencode({
+    statement_timeout = "10s"
+  })
+
   api = jsonencode({
     db_schema            = "public,storage,graphql_public"
     db_extra_search_path = "public,extensions"
