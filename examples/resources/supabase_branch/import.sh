2bfa13911207434da8227dfddc45faac78732468
Alex Savanovich <40720931+savme@users.noreply.github.com>
2026-02-17 14:27:03 +0100

    chore: update branches import doc

--- examples/resources/supabase_branch/import.sh
+++ examples/resources/supabase_branch/import.sh
@@ -1,2 +1,5 @@
-# The ID is the branch identifier.
+# Branches can be imported using the branch ID.
+#
+# - branch_id: The UUID of the branch. Retrieve it via the Supabase CLI:
+#     supabase branches list
 terraform import supabase_branch.development <branch_id>
