f2fd4e7753a51c87a8ee00c67874f3b63a12e0d9
Raminder Singh <romi_ssk@yahoo.co.in>
2026-03-10 20:33:44 +0530

    feat: parameter validation and logging
    Config model parameters are validated and better error messages are returned.
    Also logging is improved.

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -9,10 +9,12 @@ import (
 	"os"
 
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/supabase/cli/pkg/api"
 )
 
@@ -57,34 +59,69 @@ func (p *SupabaseProvider) Schema(ctx context.Context, req provider.SchemaReques
 func (p *SupabaseProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
 	var data SupabaseProviderModel
 
+	tflog.Debug(ctx, "supabase_provider configure")
+
 	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+
+	// Configuration values are now available.
+
+	// Validate endpoint
+	if data.Endpoint.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("endpoint"),
+			"Unknown Supabase API Endpoint",
+			"The provider cannot create the Supabase API client as there is an unknown configuration value for the Supbase API endpoint. "+
+				"Either target apply the source of the value first, set the value statically in the configuration, use the SUPABASE_API_ENDPOINT environment variable."+
+				", or use none of these options to let the endpoint default to https://api.supabase.com",
+		)
+	}
+	apiEndpoint := os.Getenv("SUPABASE_API_ENDPOINT")
+	if !data.Endpoint.IsNull() {
+		apiEndpoint = data.Endpoint.ValueString()
+	}
+	if apiEndpoint == "" {
+		apiEndpoint = "https://api.supabase.com"
+	}
+
+	// Validate access_token
+	if data.AccessToken.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("access_token"),
+			"Unknown Supabase API Accesss Token",
+			"The provider cannot create the Supabase API client as there is an unknown configuration value for the Supbase API accesss token. "+
+				"Either target apply the source of the value first, set the value statically in the configuration, or use the SUPABASE_ACCESS_TOKEN environment variable.",
+		)
+	}
+	accessToken := os.Getenv("SUPABASE_ACCESS_TOKEN")
+	if !data.AccessToken.IsNull() {
+		accessToken = data.AccessToken.ValueString()
+	}
+	if accessToken == "" {
+		resp.Diagnostics.AddAttributeError(path.Root("access_token"),
+			"Missing Supabase API Access Token",
+			"Set the access token using either the access_token parameter or the SUPABASE_ACCESS_TOKEN environment variable")
+	}
 
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Configuration values are now available.
-	if data.Endpoint.IsNull() {
-		data.Endpoint = types.StringValue("https://api.supabase.com")
-	}
-	if data.AccessToken.IsNull() {
-		data.AccessToken = types.StringValue(os.Getenv("SUPABASE_ACCESS_TOKEN"))
-	}
-
 	// Example client configuration for data sources and resources
 	client, _ := api.NewClientWithResponses(
-		data.Endpoint.ValueString(),
+		apiEndpoint,
 		api.WithRequestEditorFn(func(ctx context.Context, req *http.Request) error {
-			req.Header.Set("Authorization", "Bearer "+data.AccessToken.ValueString())
-			req.Header.Set("User-Agent", "TFProvider/"+p.version)
+			req.Header.Set("Authorization", "Bearer "+accessToken)
+			req.Header.Set("User-Agent", "SupabsaeTerraformProvider/"+p.version)
 			return nil
 		}),
 	)
+
 	resp.DataSourceData = client
 	resp.ResourceData = client
 }
 
 func (p *SupabaseProvider) Resources(ctx context.Context) []func() resource.Resource {
+	tflog.Debug(ctx, "supabase_provider returning resources")
 	return []func() resource.Resource{
 		NewProjectResource,
 		NewSettingsResource,
@@ -95,6 +132,7 @@ func (p *SupabaseProvider) Resources(ctx context.Context) []func() resource.Reso
 }
 
 func (p *SupabaseProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
+	tflog.Debug(ctx, "supabase_provider returning data sources")
 	return []func() datasource.DataSource{
 		NewBranchDataSource,
 		NewPoolerDataSource,
